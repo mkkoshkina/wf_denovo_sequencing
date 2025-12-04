3b1acae8e4c579b637c651e72b04f2a322a47c0f
Mariia Koshkina <mariia.koshkina@curie.fr>
2025-12-04 17:09:50 +0100

    refactor: rename rules for clarity and add resource specifications

---
+++ main/wf_sequence/workflow/profiles/soroban/todo.py
@@ -0,0 +1,5 @@
+SEQS.append(DenovoSequencing(project="test", 
+                            MS_data= '/mnt/raid0/file_buffer/test', 
+                            map_to_protein=True,
+                            extra_fasta='/mnt/raid0/ewing_sarcoma/data/databases/vibert_etal.fasta'))
+
